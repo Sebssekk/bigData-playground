a97ed04ce8c37428a23c9fd7a0ec9087693ff4db
sebssekk <sebssekk@hotmail.it>
2022-05-27 22:47:27 +0200

    updating packages (27-05-2022) and fixing dockerfile

--- cluster.sh
+++ cluster.sh
@@ -14,19 +14,20 @@ login     Exec bash commands on the specified service
 
 if [ $# -gt 0 -a ! -z "$1"  ]; then
   if [ "$1" == "start" ]; then
-    docker-compose -f ./CLUSTER/hadoop/docker-compose.yaml up -d
+    docker compose -f ./CLUSTER/hadoop/docker-compose.yaml up -d
   elif [ "$1" == "stop" ]; then
-    docker-compose -f ./CLUSTER/hadoop/docker-compose.yaml down \
-    && rm -r ./CLUSTER/hdfs-storage-space/*/*
+    docker compose -f ./CLUSTER/hadoop/docker-compose.yaml down 
+    echo "Cleaning data ... "
+    sudo rm -r ./CLUSTER/hdfs-storage-space/*/*
   elif [ "$1" == "login" ]; then
     if [ $# -gt 1 ]; then
-      docker-compose -f ./CLUSTER/hadoop/docker-compose.yaml exec "$2" bash
+      docker compose -f ./CLUSTER/hadoop/docker-compose.yaml exec "$2" bash
     else 
       echo "[ERROR] for 'login' you shoud specify a sercice."
       print_help
     fi
   else
-    docker-compose -f ./CLUSTER/hadoop/docker-compose.yaml "$@"
+    docker compose -f ./CLUSTER/hadoop/docker-compose.yaml "$@"
   fi 
 else
   print_help
